e6a01419ee0568fc7c5e28e5c8b25cf726521b3b
Santiago <santiagomanso@hotmail.com>
2021-12-15 19:00:59 +0100

    main commit, future:branching

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React, { Children, useContext } from 'react';
 import { ArrowRightIcon } from "@heroicons/react/solid";
 import { themeContext } from '../context/theme/themeContext';
 import AlertDialogSlide from './MuiDialog';
+import { Link } from 'react-scroll'
 import MyApp from './Resume'
 
 const Navbar = () => {
@@ -20,21 +21,26 @@ const Navbar = () => {
     return ( 
         <header className={`bg-${ darkmode === false ? 'gray-100': 'gray-800'} md:sticky top-0 z-10`}>
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                
+                
                 <h3 className={`title-font font-medium ${ (darkmode === false) ? 'text-black' : 'text-white' } mb-4 md:mb-0`}>
-                    <a href="#about" className="ml-3 text-xl">
-                        Juan Cruz Martinez
-                    </a>
+                    <Link to="about" smooth={true} duration={1000} className="cursor-pointer ml-3 text-xl">
+                        Santiago Manso Castro
+                    </Link>
                 </h3>
+
                 <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-                <a href="#projects" className={`mr-5 ${(darkmode === false) ? 'text-black hover:text-gray-300' : 'text-white hover:text-black' }`}>
+                <Link id='prueba' to="projects" smooth={true} duration={1000} className={`cursor-pointer mr-5 ${(darkmode === false) ? 'text-black hover:text-gray-300' : 'text-white hover:text-black' }`}>
                     Past Work
-                </a>
-                <a href="#skills" className={`mr-5 ${(darkmode === false) ? 'text-black hover:text-gray-300' : 'text-white hover:text-black' }`}>
+                </Link>
+
+                <Link to="skills" smooth={true} duration={1000} className={`cursor-pointer mr-5 ${(darkmode === false) ? 'text-black hover:text-gray-300' : 'text-white hover:text-black' }`}>
                     Skills
-                </a>
-                <a href="#testimonials" className={`mr-5 ${(darkmode === false) ? 'text-black hover:text-gray-300' : 'text-white hover:text-black' }`}>
+                </Link>
+
+                <Link  to="testimonials" smooth={true} duration={1000} className={`cursor-pointer mr-5 ${(darkmode === false) ? 'text-black hover:text-gray-300' : 'text-white hover:text-black' }`}>
                     Testimonials
-                </a>
+                </Link>
 
                 <AlertDialogSlide > {/* import fn from MuiDialog.js that renders the Slide Dialog */}
                     <MyApp /> {/* RESUME div */}
