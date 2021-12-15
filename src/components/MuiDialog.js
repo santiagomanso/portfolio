a55c7ea33b4eb4461543c1eed1f26fd39a2d8390
Santiago <santiagomanso@hotmail.com>
2021-12-15 19:57:03 +0100

    dialog #

--- src/components/MuiDialog.js
+++ src/components/MuiDialog.js
@@ -34,7 +34,7 @@ export default function AlertDialogSlide({children}) {
   return (
     <div className='prueba'>
       {/* the following is the element that will trigger the dialog, in this case its an <a>TAG</a> */}
-      <a href="#testimonials" className={`mr-5 ${(darkmode === false) ? 'text-black hover:text-gray-300' : 'text-white hover:text-black' }`} onClick={handleClickOpen}>Resume</a>
+      <a href="#" className={`mr-5 ${(darkmode === false) ? 'text-black hover:text-gray-300' : 'text-white hover:text-black' }`} onClick={handleClickOpen}>Resume</a>
       <Dialog
         fullWidth={fullWidth}
         maxWidth={maxWidth}
